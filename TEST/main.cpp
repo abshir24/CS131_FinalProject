8da9de38ce2e844b2fc5eb3498bdc06c7fb673a6
Abshir <abshirm@live.com>
2018-08-10 11:55:57 -0700

    final seating display finished now on to view ticket price and purchase

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -11,14 +11,19 @@ void retrieveSeats(char seats[][30],int size);
 
 void displaySeats(char seats[][30],int size);
 
+void viewTicketPrices(const double[],int size);
+
 int main()
 {
     char seats[15][30];
+    
+    const double ticketPrices[15] = {50,48,46,44,42,40,38,36,34,32,30,28,26,24,22};
     
     retrieveSeats(seats,15);
     
     displaySeats(seats,15);
     
+    viewTicketPrices(ticketPrices,15);
 
     return 0;
 }
@@ -65,17 +70,22 @@ void retrieveSeats(char seats[][30],int size)
 
 void displaySeats(char seats[][30],int size)
 {
-    int i,j;
+    int i,j,width;
+    double rowPrice = 50.0;
     
     cout<<"    "<<1;//5 space chars
     
-    for(i = 2;i <= 30;++i)
-        cout<<setw(5)<<i;//4 space chars
+    width = 5;
     
+    for(i = 2;i <= 31;++i)
+        //If i is less than 31 display row number else display price header
+        
+        i < 31? cout<<setw(width)<<i : cout<<setw(8)<<"Price";
+
     cout<<endl;
     cout<<"   ----|";
     
-    for(i = 2;i <= 30;++i)
+    for(i = 2;i <= 31;++i)
         cout<<"----|";
     
     cout<<endl;
@@ -85,18 +95,24 @@ void displaySeats(char seats[][30],int size)
         cout<<setw(2)<<i+1<<"|";
         
         
-        for(j = 0;j <30;++j){
+        for(j = 0;j <31;++j){
+            //If the row price is less not a double digit number then the width of box == 3
             
+            rowPrice<10? width = 3 : width = 2;
             
+            //If the count is less than 30 display the seating info else display the price of the row
             
-            cout<<setw(4)<<seats[i][j]<<"|";
+            j<30? cout<< setw(4) <<seats[i][j] <<"|" : cout<<setw(width)<< "$"<<rowPrice<<"|" ;
+            
         }
         
         cout<<endl;
         
+        rowPrice -= 2.0;
+        
         cout<<" -|----";
         
-        for(j = 2;j < 30;++j)
+        for(j = 2;j < 31;++j)
             cout<<"|----";
         
         cout<<"|----|";
@@ -104,3 +120,54 @@ void displaySeats(char seats[][30],int size)
         
     }
 }
+
+void viewTicketPrices(const double ticketPrices[],int size)
+{
+    int i,j;
+    
+    cout<<"    "<<"Row";//5 space chars
+    
+    
+    cout<<setw(8)<<"Price";
+    
+    cout<<endl;
+    cout<<"   ----|";
+    
+    for(i = 2;i <= 4;++i)
+        cout<<"----|";
+    
+    cout<<endl;
+    
+//    for(i = 0;i <size;++i)
+//    {
+//        
+//        for(j = 0;j <31;++j){
+//            
+//            
+//          
+//            
+//            //If the count is less than 30 display the seating info else display the price of the row
+//            
+//            j<30? cout<< setw(4) <<seats[i][j] <<"|" : cout<<setw(width)<< "$"<<rowPrice<<"|" ;
+//            
+//        }
+//        
+//        cout<<endl;
+//        
+//        rowPrice -= 2.0;
+//        
+//        cout<<" -|----";
+//        
+//        for(j = 2;j < 31;++j)
+//            cout<<"|----";
+//        
+//        cout<<"|----|";
+//        cout<<endl;
+//        
+//    }
+}
+
+
+
+
+
