d6b7a292c48e17e98e0677b012c4879231d9f631
Abshir <abshirm@live.com>
2018-08-10 14:29:01 -0700

    purchase ticket and update database functions complete

--- TEST/main.cpp
+++ TEST/main.cpp
@@ -13,11 +13,14 @@ void displaySeats(char seats[][30],int size);
 
 void viewTicketPrices(const double[],int size);
 
-void purchaseTicket(char*,const double*,double*);
+void purchaseTicket(char[][30],int,const double[],double[],int&,int);
 
 int numAvailableTickets(char[][30],int);
 
-void checkAvailability(char[][30],int,int[],int[],int&);
+bool checkAvailability(char[][30],int,const double[],double[],int&);
+
+void updateSeatsDb(char[][30],int size);
+
 
 int main()
 {
@@ -25,17 +28,20 @@ int main()
     
     const double ticketPrices[15] = {50,48,46,44,42,40,38,36,34,32,30,28,26,24,22};
     
-    double purchases[50];
+    double purchases[450];
     
-    int purchaseArrIdx = 0;
+    int purchasesIdx = 0;
     
     
-//    retrieveSeats(seats,15);
-//
-//    displaySeats(seats,15);
+    retrieveSeats(seats,15);
+
+    displaySeats(seats,15);
     
 //    viewTicketPrices(ticketPrices,15);
     
+    purchaseTicket(seats,15,ticketPrices,purchases,purchasesIdx,numAvailableTickets(seats, 15));
+    
+    displaySeats(seats,15);
     
 
     return 0;
@@ -46,15 +52,15 @@ int main()
 void retrieveSeats(char seats[][30],int size)
 {
     
-    ifstream readIn;
+    ifstream readFile;
     
-    readIn.open("db.txt");
+    readFile.open("db.txt");
     
     char seat;
     
     int row=0,column=0;
     
-    while(readIn>>seat){
+    while(readFile>>seat){
         
         seats[row][column++] = seat;
         
@@ -66,6 +72,8 @@ void retrieveSeats(char seats[][30],int size)
         }
         
     }
+    
+    readFile.close();
 }
 
 //This function takes the newly updated 2D array of seats and organizes it to chart of seats.
@@ -157,7 +165,7 @@ void viewTicketPrices(const double ticketPrices[],int size)
 
 //Function for purchasing ticket.
 
-void purchaseTicket(char seats[][30],const double prices[],double purchases[],int availableTickets,int seatsSize, int &purchasesIdx)
+void purchaseTicket(char seats[][30],int seatsSize,const double prices[],double purchases[],int &purchasesIdx,int availableTickets)
 {
     int ticketAmount;
     
@@ -166,18 +174,39 @@ void purchaseTicket(char seats[][30],const double prices[],double purchases[],in
     do{
         cout<<"The number of available tickets are " << availableTickets<<endl;
         
-        cout<<"How many tickets would you like to purchase? (Ticket amount must be greater than 0 and less than " << availableTickets <<endl;
+        cout<<"How many tickets would you like to purchase? (Ticket amount must be greater than 0 and less than " << availableTickets << ")"<<endl;
         
         cin>>ticketAmount;
+        
     }while(ticketAmount < 1 || ticketAmount > availableTickets);
     
-    
+    cout<<"Ticket Amount "<<e
     //For loop runs for the number of tickets that the user wants to buy
     
     for(int i = 0;i<ticketAmount;i--)
     {
         cout<<"Select a seat for ticket#" << (ticketAmount - i)<<endl;
         
+        //If no seat is purchased we will ask the user if they still want to purchase the remaining amount of tickets that they selected
+        
+        if(!checkAvailability(seats, seatsSize, prices, purchases, purchasesIdx)){
+            
+            string answer;
+            
+            cout<<"Would you like to purchase the remaining "<< (ticketAmount - (i+1)) << " tickets? (yes or no)"<<endl;
+            
+            cin>>answer;
+            
+            //If the user doesn't want to buy their remaining tickets then we will redirect them to the home page.
+            
+            if(answer[0] == 'N' || answer[0] == 'n'){
+                
+                cout<<"We will now return you to the main menu thanks for shopping with us"<<endl;
+                
+                return;
+            }
+            
+        }
         
     }
     
@@ -187,7 +216,7 @@ void purchaseTicket(char seats[][30],const double prices[],double purchases[],in
 
 //Function to check to see if a seat is available
 
-void checkAvailability(char seats[][30],int seatsSize,int prices[],int purchases[],int &purchasesIdx)
+bool checkAvailability(char seats[][30],int seatsSize,const double prices[],double purchases[],int &purchasesIdx)
 {
     int row,seat;
     
@@ -199,6 +228,9 @@ void checkAvailability(char seats[][30],int seatsSize,int prices[],int purchases
         cin>>row;
     }while(row<1 || row>15);
     
+    //Decrement the row size to fit array index
+    row--;
+    
     //This loop asks the user for the row of the ticket that they want to purchase runs until number is  between 1 and 30
     do
     {
@@ -207,14 +239,71 @@ void checkAvailability(char seats[][30],int seatsSize,int prices[],int purchases
         cin>>seat;
     }while(seat<1 || seat>30);
     
+    //Decrement the seat size to fit array index
+    seat--;
+    
+    //If the seat is available
     if(seats[row][seat] == '#')
     {
+        //Display seat price
         cout << "This seat is available. The price for this seat is $" << prices[row]<<endl;
         
-        purchases[purch]
+        //Add the purchase price to the purchases array and increment the index of the purchases array
         
+        purchases[purchasesIdx++] =prices[row];
+        
+        //Update the array to make the seat unavailable
+        
+        seats[row][seat] = '*';
+        
+        //Update the database
+        
+        updateSeatsDb(seats,seatsSize);
         
     }
+    else{
+        //If the seat is not available ask the user if they want to purchase another ticket
+        string answer;
+        
+        cout<<"This seat is not available would like to purchase another seat in its place? (yes or no)"<<endl;
+        
+        cin>> answer;
+        
+        //If the user answers yes then run the check availability function again
+        if(answer[0] == 'Y' || answer[0] == 'y')
+            
+            checkAvailability(seats,seatsSize,prices,purchases,purchasesIdx);
+        
+        else
+            //return false to send them back to ticket purchases
+            return false;
+            
+    }
+    
+    cout<<"Congratulations seat #"<< row <<","<< seat <<" is currently yours!"<<endl;
+    
+    return true;
+}
+
+//Updates the seat data in the text file.
+
+void updateSeatsDb(char seats[][30],int size)
+{
+    ofstream writeFile;
+    
+    writeFile.open("db.txt");
+    
+    for(int i = 0;i<size;i++){
+        
+        for(int j = 0;j<30;j++)
+            
+            writeFile<<seats[i][j];
+        
+        writeFile<<endl;
+    }
+    
+    writeFile.close();
+    
 }
 
 //Function for checking the number of available tickets
@@ -223,9 +312,14 @@ int numAvailableTickets(char arr[][30],int size)
     int count = 0;
     
     for(int i = 0;i<size;i++)
+        
         for(int j = 0;j<30;j++)
+            
             //Check to see if the seat is available and increment count if it is.
+            
             if(arr[i][j] == '#') count++;
+    
+    return count;
 }
 
 
